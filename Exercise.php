530984092b6d5bd4192f38e0c8ee3d6141e66472
oakwoodyk <82461834+oakwoodyk@users.noreply.github.com>
2021-07-03 12:50:27 +0900

    Add files via upload

---
+++ Exercise.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Exercise extends Model
+{
+    use HasFactory;
+
+    public static $rules = array(
+        'email' => 'required',
+    );
+}
