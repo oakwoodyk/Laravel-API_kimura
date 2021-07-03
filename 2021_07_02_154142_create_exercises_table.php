6feec8f1b47fe6306613910075e3d05d36664d90
oakwoodyk <82461834+oakwoodyk@users.noreply.github.com>
2021-07-03 12:51:18 +0900

    Add files via upload

---
+++ 2021_07_02_154142_create_exercises_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExercisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exercises', function (Blueprint $table) {
+            $table->string('name')->required();
+            $table->string('email')->required()->unique();;
+            $table->string('profile')->required();
+            $table->timestamp('created_at')->useCurrent()->nullable();
+            $table->timestamp('updated_at')->useCurrent()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exercises');
+    }
+}
